bd61a77e2f96f60a6bf0f3a8c61cd69bc1bf1f40
cryptoquick <cryptoquick@gmail.com>
2013-02-10 13:12:30 -0700

    union is looking good, but I'd like to write more on it.

--- index.js
+++ index.js
@@ -1,21 +1,103 @@
 
 module.exports = function (opts) {
-	if (!opts) opts = {}
+	var opts = opts || {};
+	opts.type = opts.type || 'full';
+	opts.vals = opts.vals || 'same';
 
-	this.union = function (a, b) {
-		var searchRuns = 0;
-		var searched = [];
-		var curCallStack = 0;
-		var maxCallStack = 0;
+	var ro = {}; // result object
 
-		
+	var hash = function (ex) {
+		var size = opts.size || 32;
+		return ex[0] * size * size + ex[1] * size + ex[2]; // hash index = x, y, z
 	}
 
-	this.difference = function (a, b) {
-		
+	// var sameUnion = function () {
+
+	// }
+
+	var fullUnion = function (a, b) {
+		// var ufunc = 
+		var result = [];
+
+		if (!a || !b) {
+			console.error('missing either a or b', 'a length:', a, 'b length:', b);
+		}
+
+		for (var aa = 0, aaa = a.length; aa < aaa; aa++) {
+			var ah = hash(a[aa]);
+			if (ro[ah]) {
+				ro[ah].push(a[aa]);
+			}
+			else {
+				ro[ah] = [a[aa]];
+			}
+		}
+
+		for (var bb = 0, bbb = b.length; bb < bbb; bb++) {
+			var bh = hash(b[bb]);
+			if (ro[bh])
+				ro[bh].push(b[bb]);
+			else
+				ro[bh] = [b[bb]];
+		}
+
+		return ro;
 	}
 
-	this.intersection = function (a, b) {
+	var full
 
+
+	/*	// turn voxel models into hash tables using hashed position indices
+		var ao = {}, bo = {};
+
+		for (var aa = 0, aaa = a.length; aa < aaa; aa++) {
+			ao[hash(a[aa])] = a[aa];
+		}
+
+		for (var bb = 0, bbb = b.length; bb < bbb; bb++) {
+			bo[hash(b[bb])] = b[bb];
+		}
+
+
+
+		// union
+		for (var ai = 0, aii = a.length; ai < aii; ai++) {
+			ao[]
+		}
+
+		for (var i = 0, ii = Math.max(a.length, b.length); i < ii; i++) {
+			// union
+
+
+			if (!ai[i]) {
+				result.push(bi);
+			}
+			else if (!bi) {
+				result.push(ai);
+			}
+			else if (ai == bi) {
+				result.push(ai);
+				console.log(ai);
+			}
+			else {
+				result.push(ai);
+				result.push(bi);
+			}
+		}
+
+		return result;
+	}*/
+
+	switch (opts.type) {
+		case 'full':
+			this.union = fullUnion;
+			// this.difference = fullDifference;
+			// this.intersection = fullIntersection;
+			break;
+		case 'smart':
+			// this.union = smartUnion;
+			// this.difference = smartDifference;
+			// this.intersection = smartIntersection;
+			break;
 	}
 }
--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-var csg = require('voxel-csg')
+var csg = new module.exports();
 
- 
+var modelA = 1;
+var modelB = 2;
+
+var date1 = new Date();
+var unionResult = csg.union(models[modelA], models[modelB]);
+var date2 = new Date();
+
+console.log('a length:', models[modelA].length, 'b length:', models[modelB].length,
+	'union length:', Object.keys(unionResult).length, 'a length + b length', models[modelA].length + models[modelB].length);
+console.log('union took', date2 - date1, 'ms to complete.');
